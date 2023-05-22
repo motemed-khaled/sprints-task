bd7ca08dd3ecf1aacc790e1be45a880791932e30
motemed khaled <motemedkhaled5@gmail.com>
2023-05-22 18:56:03 +0300

    first sprint

---
+++ js/main.js
@@ -0,0 +1,3 @@
+document.querySelector(".first-nav .right .account").addEventListener("click", () => {
+    document.querySelector(".first-nav .right .account .drop-list").classList.toggle("show");
+});
